21643d8d783eba3802b21ae9af43ec90b2f6fe8b
naczyz <natan.czyz@gmail.com>
2023-09-08 16:07:50 +0200

    poprawa zadania

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const getData = () => {
 
           pId.innerText = `User ID: ${user.id}`;
           pName.innerText = `User Name: ${user.name}`;
-          pWebsite.innerText = `User URL: ${user.website}<br/>-------------`;
+          pWebsite.innerHTML = `User URL: ${user.website}<br/>-------------`;
 
           body.appendChild(pId);
           body.appendChild(pName);
